40880022423808183233d94d78d7d12068075ceb
DESKTOP-EH362V9\Jachaś <sc00r398@gmail.com>
2017-06-19 08:00:58 +0200

    fixed_this

--- board.py
+++ board.py
@@ -13,14 +13,14 @@ class Board(object):
             self.players = players
 
     def render_players(self):
-        for i in range(0, self.players - 1):
+        for i in range(0, self.players):
             self.player.append(Player(i))
 
     def return_players(self):
         return self.player
 
     def check_collision(self, pawn):
-        for i in range(0, self.players - 1):
+        for i in range(0, self.players):
             for p in range(0, 3):
                 if not pawn.pawn_id == self.player[i].player_pawns[p].pawn_id:
                     if pawn.pawn_position == self.player[i].player_pawns[p].pawn_position:
@@ -35,15 +35,13 @@ class Board(object):
             pionek = input()
             if 4 > int(pionek) > 0:
                 moved = True
-                self.player[self.player_turn].player_pawns[int(pionek)].set_pawn_position(roll)
                 self.next_player_turn()
+                self.player[self.player_turn].player_pawns[int(pionek)].set_pawn_position(roll)
 
     def next_player_turn(self):
-        if self.player_turn + 1 == self.players:
+        if self.player_turn > 3:
             self.player_turn = 0
-            self.next_player_turn()
-        else:
-            self.player_turn += 1
+        self.player_turn += 1
 
     def get_player_turn(self):
         return self.player_turn
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from board import Board
 
 game = Board(4)
 game.render_players()
+print(game.player)
 while True:
     print(game.get_player_turn())
     print(game.player_turn)
